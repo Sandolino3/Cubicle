22365937f1ee18320e08c708d4799411f8089c82
Sandolino3 <sandolino6@gmail.com>
2022-06-04 10:43:55 +0300

    Separete controlers

--- src/controlers/homeControler.js
+++ src/controlers/homeControler.js
@@ -2,10 +2,5 @@ exports.index = (req,res)=>{
 res.render('index')
 }
 
-exports.about = (req,res)=>{
-    res.render('about')
-}
 
-exports.create = (req,res)=>{
-    res.render('create')
-}
+
--- src/views/routes.js
+++ src/views/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const homeControler = require('../controlers/homeControler')
+const aboutControler = require('../controlers/aboutControler')
+const createControler = require('../controlers/createControler')
 
 router.get('/',homeControler.index)
-router.get('/about',homeControler.about)
-router.get('/create',homeControler.create)
+router.get('/about',aboutControler.about)
+router.get('/create',createControler.create)
 
 
 module.exports=router;
