e7fcbe589a065484ae53e5508f539a858f03e61d
Sandolino3 <89142522+Sandolino3@users.noreply.github.com>
2022-07-08 14:24:07 +0300

    add dynamic nav bar

--- SRC/midlewares/authMidleware.js
+++ SRC/midlewares/authMidleware.js
@@ -11,6 +11,7 @@ exports.auth = async (req,res,next)=>{
         try {
             let decodetToken = await jwtVerify(token, secret)
             req.user = decodetToken
+            res.locals.user = decodetToken
         } catch (error) {
             return res.redirect('/404')
         }
@@ -26,9 +27,9 @@ exports.isAuth = (req, res, next)=>{
    next()
 }
 
-exports.isOwner = (req,res,next) =>{
-    if(req.user._id !== req.params.id){
-       return res.redirect('/404')
-    };
-    next()
-}
+// exports.isOwner = (req,res,next) =>{
+//     if(req.user._id !== req.params.id){
+//        return res.redirect('/404')
+//     };
+//     next()
+// }
